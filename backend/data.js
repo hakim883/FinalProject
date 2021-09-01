6dce91e70e6c61a5bc049fa2e9d3f83e0c744236
hakim <hakimchaabani6@gmail.com>
2021-09-01 13:41:56 +0200

    second commit

---
+++ backend/data.js
@@ -0,0 +1,93 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'hakim',
+            category: 'sirus',
+            image:'/images/p1.jpg',
+            prix: 120,
+            countInStock:10,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani',
+        },
+        {
+            _id:'2',
+            name:'hakim2',
+            category: 'sirus',
+            image:'/images/p2.jpg',
+            prix: 50,
+            countInStock:0,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani2',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+        {
+            _id:'3',
+            name:'hakim3',
+            category: 'sirus',
+            image:'/images/p3.jpg',
+            prix: 78,
+            countInStock:8,
+            logo_aem:'aem',
+            
+            desc:'hakim chaabani3',
+        },
+    ],
+};
+export default data
